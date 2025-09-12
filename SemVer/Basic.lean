d7c34c9d31d4edff2209c8bbf3e1ec584dbdf217
Dr. Stefan Kusterer <stefan.kusterer@gmx.net>
2025-09-12 18:36:09 +0200

    add copyright

--- SemVer/Basic.lean
+++ SemVer/Basic.lean
@@ -1,3 +1,7 @@
+/-
+Copyright (c) 2025 Stefan Kusterer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
 section ParserErrors
 
 structure ParserError where
